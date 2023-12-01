0e1d534f23306e17d9c40c39b608038039eaea61
Shri Dharshini <85439398+shri-dharshini@users.noreply.github.com>
2023-12-01 19:03:18 +0530

    added comments

--- DLL_interview.py
+++ DLL_interview.py
@@ -31,10 +31,10 @@ class DoubleLinkedList:
             return
         temp=self.head
         while temp:
-            after=temp.next
-            temp.next=temp.prev
-            temp.prev=after
-            temp=after
+            after=temp.next #store next node
+            temp.next=temp.prev #assign next node as prev node
+            temp.prev=after #assign prev node as the stored next node, effectively reversing links for the current node
+            temp=after #go to the next node for iteration
         self.tail,self.head=self.head,self.tail
 
     #from the tail end we have to go to previous
@@ -44,29 +44,32 @@ class DoubleLinkedList:
         temp1=self.head
         temp2=self.tail
         for _ in range(self.length//2):
-            if temp1.value==temp2.value:
+            if temp1.value==temp2.value: #compare first and last values as we move closer to the middle
                 temp1=temp1.next
                 temp2=temp2.prev
             else:
                 return False
         return True
     
+
+    #swap pairs of nodes, 1<->2<->3<->4<->5<->6 becomes 2<->1<->4<->3<->6<->5
     def swap_pairs(self):
-        dummy=Node(0)
+        dummy=Node(0)#sentinel or placeholder node, handles edge cases
+        #This eliminates the need for special cases when the reversal starts from the head
         dummy.next=self.head
         prev_node=dummy
         while self.head and self.head.next:
-            first_node=self.head
+            first_node=self.head #take 2 nodes at a time
             second_node=self.head.next
-            prev_node.next=second_node
-            first_node.next=second_node.next
+            prev_node.next=second_node 
+            first_node.next=second_node.next #reverses link in single direction
             second_node.next=first_node
-            second_node.prev=prev_node
-            first_node.prev=second_node
+            second_node.prev=prev_node 
+            first_node.prev=second_node #reverses link completely
             if first_node.next:
-                first_node.next.prev=first_node
+                first_node.next.prev=first_node #Ensure that the node after our swapped pair has its prev updated
             self.head=first_node.next
             prev_node=first_node
-        self.head=dummy.next
+        self.head=dummy.next #reassign head
         if self.head:
             self.head.prev=None
